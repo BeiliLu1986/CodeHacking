87159af64566fb6e5e37fb9e3bbadefff41accf8
Luis E <bellyluis@gmail.com>
2018-12-06 16:29:44 -0500

    Update user form working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -10,6 +10,7 @@ use App\User;
 use App\Role;
 use App\Photo;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UserEditRequest;
 
 class AdminUsersController extends Controller
 {
@@ -115,9 +116,31 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
         //
+        
+        $user = User::findOrFail($id);
+
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images',$name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
     }
 
     /**
