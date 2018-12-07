d584761a434e46f0e7c889afed2e727a4d9daa53
Luis E <bellyluis@gmail.com>
2018-12-07 15:58:55 -0500

    Middleware Admin

--- app/User.php
+++ app/User.php
@@ -36,4 +36,27 @@ class User extends Authenticatable
         return $this->belongsTo('App\Photo');
     }
 
+    public function setPasswordAttribute($password){
+
+        if(!empty($password)){
+
+            $this->attributes['password'] = bcrypt($password);
+        }
+
+    }
+
+    public function isAdmin(){
+
+        if($this->role->name == "administrator"){
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+
+
 }
