1463d7991d80b690c1cc7bdbf4c43d565e368971
Luis E <bellyluis@gmail.com>
2018-12-07 19:25:33 -0500

    Creating form part 2

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -43,6 +43,9 @@ class AdminPostsController extends Controller
     public function store(Request $request)
     {
         //
+
+        return $request->all();
+
     }
 
     /**
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,7 +4,8 @@
 
 <h1>Create Post</h1>
 
-    {!! Form::open(['method'=>'POST','action'=>'AdminPostsController@store', 'files'=>true]) !!}
+<div class="row">
+        {!! Form::open(['method'=>'POST','action'=>'AdminPostsController@store', 'files'=>true]) !!}
         <div class="form-group">
             {!! Form::label('title','Title:') !!}
             {!! Form::text('title', null, ['class'=>'form-control']) !!}
@@ -26,5 +27,13 @@
             {!! Form::submit('Create Post',['class'=>'btn btn-primary']) !!}
         </div>
     {!! Form::close() !!}
+</div>
+
+<div class="row">
+        @include('includes.form_createUser_error')  
+</div>
+  
+
+     
 
 @stop
