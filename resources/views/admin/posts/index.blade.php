dfcf7a391cd54593d984dd6fce4933dbcf7906e5
Luis E <bellyluis@gmail.com>
2018-12-10 12:03:00 -0500

    Edit posts working

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -24,7 +24,7 @@
                     <tr>
                         <td>{{ $post->id }}</td>
                         <td><img height="50" src="{{ $post->photo ? $post->photo->file : '/images/user.jpg'}}"></td>
-					    <td>{{ $post->user->name}}</td>
+					    <td><a href="{{ route('admin.posts.edit', $post->id ) }}">{{ $post->user->name}}</a></td>
                         <td>{{ $post->category ? $post->category->name : 'Uncategorized' }}</td>
                         <td>{{ $post->title }}</td>
                         <td>{{ $post->body}}</td>
